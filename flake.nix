62dab720e9a700100ba8a65c3bc17b17bb883ec3
Tung Le <tungle@rintung-mac.local>
2025-05-16 19:57:58 -0700

    set up

--- flake.nix
+++ flake.nix
@@ -7,14 +7,17 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, ... }:
   let
-    
+    system = "aarch64-darwin";
+    specialArgs =
+      inputs;
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#rintung-mac
     darwinConfigurations."rintung-mac" = nix-darwin.lib.darwinSystem {
+      inherit system specialArgs;
       modules = [ 
         ./modules/system
       ];
